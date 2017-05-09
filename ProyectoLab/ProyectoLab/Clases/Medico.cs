34bdd5fa8d5f8ca8b6c62494ab9e1c4a1fc89692
Nestor <alexdmerida@gmail.com>
2017-05-08 21:51:38 -0600

    << Nestor - Historia Ver Citas>>
    "Medico - Ver Citas"

---
+++ ProyectoLab/ProyectoLab/Clases/Medico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using ProyectoLab.Servicios;
+namespace ProyectoLab.Clases
+{
+    public class Medico
+    {
+        BDService serviciosBD = new BDService();
+        public DataTable verDatosMedico(string dpiMedico)
+        {
+
+            DataTable tabla = serviciosBD.FillTableData("SELECT Dpi, Nombre, Fecha_Nacimiento, Telefono, Email, Tarjeja from dbo.Usuario WHERE Dpi=" + dpiMedico + " and cod_tipo = 2");
+            return tabla;
+        }
+        
+    }
+}
